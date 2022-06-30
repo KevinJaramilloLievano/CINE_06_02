50d63c875098f0f6cd76e3c32dc42053646f72a7
Diego <brayan.67diego@gmail.com>
2022-06-30 10:31:49 -0500

    Add: error-404

---
+++ 404.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Y las pelis?</title>
+    <link rel="stylesheet" href="./css/styles-err.css">
+</head>
+<body>
+    <h1 class="error">Error 404</h1>
+    <h3 class="sub-error">Al parecer estamos desubicados del lugar donde nace la magia o la direccion que buscas no existe :c</h3>
+
+    <div>
+        <a href="./index.php" class="btn">Regresar a la pagina principal</a>
+    </div>
+
+</body>
+</html>
