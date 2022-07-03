1b18fcd6e6b690d9fa743a667a4760e42d47b4c4
miiky976 <mruiz3445@gmail.com>
2022-07-03 00:29:12 -0500

    header y footer para facilitar el trabajo

---
+++ EjemploHeaderFooter.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/estiloCine1.css">
+    <title>Titulo</title>
+</head>
+<body>
+    <?php include('header.php'); ?>
+    <h1>Aqui se agregara el contenido</h1>
+    <h2>Este es solo un ejemplo</h2>
+    <?php include('footer.php'); ?>
+</body>
+</html>
