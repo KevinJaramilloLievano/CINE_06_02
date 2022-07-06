4dbd195c1e2fc406644b472e8c526f578f4eb6ab
miiky976 <58113452+miiky976@users.noreply.github.com>
2022-07-05 21:59:04 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,17 @@
-<!-- Aqui estaria bien agregar un footer real-->
-<!-- Aunque no veo la necesidad de uno XD -->
-<!-- Las clases las agregue para resaltarlo -->
-<footer class="bg-dark text-white">
-    <h1>Sustituir por un buen footer XD</h1>
+<!-- .-. -->
+<script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
+<footer class="bg-dark text-white py-4">
+    <div class="container">
+        <nav class="row">
+            <h3 class="col-3 text-uppercase text-reset d-flex align-items-center">Cinema UPP</h3>
+            <ul class="col-3 list-unstyled">
+                <li class="font-weight-bold text-uppercase">Nuestras Redes Sociales</li>
+                <li class="d-flex justify-content-between">
+                    <a class="text-reset" href="#"><span class="iconify" data-icon="akar-icons:facebook-fill"></span></a>
+                    <a class="text-reset" href="https://youtu.be/dQw4w9WgXcQ" target="_blank"><span class="iconify" data-icon="akar-icons:youtube-fill"></span></a>
+                    <a class="text-reset" href="#"><span class="iconify" data-icon="akar-icons:instagram-fill"></span></a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </footer>
