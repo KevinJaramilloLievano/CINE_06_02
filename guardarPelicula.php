81fe56644b1f4a99705048914920c76e4eed020f
jcrv909 <blaze.uno.909@gmail.com>
2022-07-07 02:21:04 -0500

    Se implementaron validaciones al momento de registrar una nueva película y se corrigió la inserción de la fecha

--- guardarPelicula.php
+++ guardarPelicula.php
@@ -1,39 +1,51 @@
-<!DOCTYPE html>
-<html>
-    <head>
 <?PHP
 
-	$nombreP = $_POST["nombrePelicula"];
-	$fecha = $_POST["fecha"];
-	$nac = $_POST["nacionalidad"];
-	$idioma = $_POST["idioma"];
-	$color = $_POST["color"];
-	$clasi = $_POST["clasificacion"];
-	$sin = $_POST["sinopsis"];
-	/*echo "La pelicula se llama ".$nombreP."<br>";
+$nombreP = $_POST["nombrePelicula"];
+
+//$fecha = $_POST["fecha"];
+
+//se crea el objeto fecha a partir de un formato y un campo/interfaz 
+$fecha = DateTime::createFromFormat('Y-m-d', $_POST['fecha']);
+//se da formato para ser utilizado en la consulta
+$fechaFormat = $fecha->format('Y-m-d');
+
+$nac = $_POST["nacionalidad"];
+$idioma = $_POST["idioma"];
+$color = $_POST["color"];
+$clasi = $_POST["clasificacion"];
+$sin = $_POST["sinopsis"];
+
+/*echo "La pelicula se llama ".$nombreP."<br>";
 	echo "La fecha es ".$fecha."<br>";
 	echo "Nacionalidad: ".$nac."<br>Idioma: ".$idioma."<br>";
 	echo "Color: ".$color."<br>Clasificacion: ".$clasi."<br>";
 	echo "Sinopsis: ".$sin;*/
 
-	//Obtien las variables del archivo externo
-	include('conexions.php');
-	//4 parámetros: servidor, usuario, contraseña y BD
-	$conexion = new mysqli($serverName,$userName,$password, $dbName);
-	$consulta = "INSERT INTO pelicula (idPelicula, nombrePelicula, fecha, nacionalidad, idioma, ColorPelicula, Clasificacion, sinopsis, estatus) 
-	VALUES (null,'$nombreP', $fecha, '$nac', '$idioma', '$color', '$clasi', '$sin', 1)";
-	$ejecutar = mysqli_query($conexion,$consulta);
+//Obtien las variables del archivo externo
+include('conexions.php');
+//4 parámetros: servidor, usuario, contraseña y BD
+$conexion = new mysqli($serverName, $userName, $password, $dbName);
+$consulta = "INSERT INTO pelicula (idPelicula, nombrePelicula, fecha, nacionalidad, idioma, ColorPelicula, Clasificacion, sinopsis, estatus) 
+	VALUES (null,'$nombreP', '$fechaFormat', '$nac', '$idioma', '$color', '$clasi', '$sin', 1)";
+$ejecutar = mysqli_query($conexion, $consulta);
 
-	echo"<script>alert('guadrdado');</script>";
+echo "Registro correcto <br>";
+echo $fechaFormat;
+echo "<a href='peliculas.php'>Volver</a>";
+
+
+echo "<script>alert('Guardado');</script>";
 ?>
 <script type="text/javascript">
-        function redirect() {
-            window.location = "peliculas.php";
-        }
-        window.onload = redirect;
-        </script>
+	function redirect() {
+		window.location = "peliculas.php";
+	}
+	window.onload = redirect;
+</script>
 </head>
-    <body>
-    
-    </body>
+
+<body>
+
+</body>
+
 </html>
--- nuevaPelicula.php
+++ nuevaPelicula.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<title>Nueva pelicula</title>
@@ -7,20 +8,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/estiloCine1.css">
 </head>
-<script type="text/javascript">
-	function Validacion()
-	{
-		var Confirmacion = confirm("estas seguro de que los datos son correctos?");
-		if (Confirmacion==true)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-</script>
+
 <body>
 	<div class="container centrado">
 		<h2 class="letraBlanca">Agregar nueva pelicula</h2>
@@ -33,10 +21,11 @@
 				</div>
 
 				<div class="col-lg-8">
-					<input class="form-control" name="nombrePelicula" type="text" placeholder="Nombre de la pelicula">
+					<input class="form-control" name="nombrePelicula" type="text" placeholder="Nombre de la pelicula" required />
 				</div>
+
 				<div class="col-lg-4">
-					<input class="form-control" type="date" name="fecha">		
+					<input class="form-control" type="date" name="fecha" required />
 				</div>
 
 				<div class="col-lg-12">
@@ -44,41 +33,13 @@
 				</div>
 
 				<div class="col-lg-6">
-					<input class="form-control" type="text" name="nacionalidad" placeholder="Nacionalidad">
+					<input class="form-control" type="text" name="nacionalidad" placeholder="Nacionalidad" required />
 				</div>
 				<div class="col-lg-3">
-					<input class="form-control" type="text" name="idioma" placeholder="Idioma">
+					<input class="form-control" type="text" name="idioma" placeholder="Idioma" required />
 				</div>
-				
 				<div class="col-lg-3">
-				
-				
-				<?php 
-
-				$conexion = new mysqli("localhost","root","","peliculas");
-                $query=mysqli_query($conexion,"SELECT id, color FROM color");
-		
-		?>
-
-
-
-					<select class="form-control" name="color">
-						<?php    
-						 while($color = mysqli_fetch_array($query)){
-
-						 
-
-						?>
-						<option name="valorColor" value="<?php  echo $color['color']?>"><?php echo $color['color']?></option>
-						
-						 <?php
-						   }
-						   
-						 ?>
-
-                    </select>
-
-
+					<input class="form-control" type="text" name="color" placeholder="Color" required />
 				</div>
 
 				<div class="col-lg-12">
@@ -86,11 +47,11 @@
 				</div>
 
 				<div class="col-lg-4">
-					<input type="text" class="form-control" name="clasificacion" placeholder="Clasificacion">
+					<input type="text" class="form-control" name="clasificacion" placeholder="Clasificacion" required />
 				</div>
 
 				<div class="col-lg-8">
-					<textarea class="form-control" name="sinopsis" placeholder="Sinopsis" rows="3"></textarea>
+					<textarea class="form-control" name="sinopsis" placeholder="Sinopsis" rows="3" required /></textarea>
 				</div>
 
 				<div class="col-lg-12">
@@ -98,37 +59,19 @@
 				</div>
 
 				<div class="col-lg-10">
-					
+
 				</div>
 
 				<div class="col-lg-2">
-<<<<<<< HEAD
-					<input type="submit" class="btn btn-primary" onclick ="return Validacion()" value="Guardar">
-=======
-					<input type="submit" class="btn btn-primary" value="Guardar" onclick="return crear()">
->>>>>>> 3b74a6788961c376f02bce9de9985eac2378698e
+					<input type="submit" class="btn btn-primary" value="Guardar">
 				</div>
 
 
 			</div>
-			
-			
+
+
 		</form>
 	</div>
-	<script type="text/javascript">
-		function crear(){
-
-			var respuesta = confirm("esta seguro que desea agregar esta nueva pelicula");
-			if (respuesta == true){
-              
-				
-				return true;
-			}
-			else{
-				return false;
-			}
-		}
-		
-		</script>
 </body>
+
 </html>
