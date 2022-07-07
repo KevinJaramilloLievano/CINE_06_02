58fe29f3a255a6f25bd2c86aabec8af2f03824a1
Jonathan Marquez <jmp120801@micorreo.upp.edu.mx>
2022-07-06 23:10:18 -0500

    Principios de usabilidad

--- nuevoActor.php
+++ nuevoActor.php
@@ -7,6 +7,20 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/estiloCine1.css">
 </head>
+<script type="text/javascript">
+	function Confirmar()
+	{
+		var Confirmacion = confirm("estas seguro de que los datos son correctos?");
+		if (Confirmacion==true)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+</script>
 <body class="NA">
 	<div class="container centrado">
 		<h2 class="letraBlanca">Agregar un nuevo actor</h2>
@@ -42,7 +56,7 @@
 				</div>
 
 				<div class="col-lg-2">
-					<input type="submit" class="btn btn-primary" value="Guardar">
+					<input type="submit" class="btn btn-primary" onclick ="return Confirmar()" value="Guardar">
 				</div>
 
 			</div>
