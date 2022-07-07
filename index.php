feb537bcd2dbbf81a85082cb8388cb791aec8af8
OwenSJD <owenjimenez@micorreo.upp.edu.mx>
2022-07-07 09:44:29 -0500

    Mejora de la interfaz de inicio mediante la incorporación de widgets

--- index.php
+++ index.php
@@ -1,26 +1,251 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<title>Cine</title>
 	<!-- CSS only -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/estiloCine1.css">
+   
 </head>
 <body>
+    <header>
+	 <div class="container centrado">
+		<h2 class="letraBlanca">Bienvenido a Cinema UPP</h2>
+		</div>
+	 </div>
+	<header>
 	
-	<ul class="menu">
-		<li><a href="index.php">Inicio</a></li>
-		<li><a href="peliculas.php">Peliculas</a></li>
-		<li><a href="actores.php">Actores</a></li>
-		<li><a href="´directores.php">Directores</a></li>
-		<li><a href="acerca de.php">Acerca de</a></li>
-	</ul>
+	
+	
+	<a class="botonP" href="peliculas.php">Mostrar Peliculas</a>
+	<main>
+	 <div class="pelicula-principal">
+			<div class="container centrado3">
+				<h3 class="titulo">Animales Fantasticos </h3>
+				<p class="descripcion">
+				Nueva York, año 1926. El excéntrico magizoólogo Newt Scamander (Eddie Redmayne) acaba de ser expulsado de Hogwarts debido a un incidente relacionado con una de sus criaturas mágicas. El joven mago decide entonces emprender un viaje por el mundo en busca de nuevas variedades de criaturas extraordinarias.
+				</p>
+				<button role="button" class="boton"><i class="fas fa-play"></i>Seguir Viendo</button>
+				<button role="button" class="boton"><i class="fas fa-info-circle"></i>Más Información</button>
+			</div>
+		</div>
 
-	<?php include('header.php'); ?>
-	<div class="text-center">
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/o3e-iK1ex1s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-	</div>
-	<?php include('footer.php'); ?>
+		
+		<div class="peliculas-recomendadas centrado3">
+	        <div class="container centrado3-titulo-controles">
+				<h3>Películas Recomendadas</h3>
+				<div class="indicadores"></div>
+			</div>
+			<div class="container centrado3-principal">
+				<button role="button" id="flecha-izquierda" class="flecha-izquierda"><i class="fas fa-angle-left"></i></button>
+
+				
+				<div class="container centrado3-carousel">
+					<div class="carousel">
+						<div class="pelicula">
+							<a href="#"><img src="temp/2.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/1.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/3.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/5.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/4.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/7.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/6.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/9.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/10.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/8.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/2.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/1.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/3.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/2.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/1.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/3.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/5.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/4.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/7.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/6.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/9.jpg" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="temp/10.jpg" alt=""></a>
+						</div>
+					</div>
+				</div>
+
+				<button role="button" id="flecha-derecha" class="flecha-derecha"><i class="fas fa-angle-right"></i></button>
+			</div>
+		</div>
+	 </main>
+	 
+	 <a class="botonA" href="actores.php">Mostrar Actores</a>
+		
+
+        <main>
+            <div class="Actor-momento">
+			    <div class="container centrado4">
+				 <h4 class="titulo">Dwayne “La Roca” Johnson </h4>
+				 <p class="descripcion">
+				    Dwayne Douglas Johnson es un actor y luchador profesional estadounidense, conocido popularmente como "La Roca" o "The Rock". Se desempeñó como luchador profesional para la WWE hasta su retirada oficial en 2019, con el objetivo de centrarse en su carrera artística.
+				 </p>
+				 <button role="button" class="boton2"><i class="fas fa-info-circle"></i>Más Información</button>
+			    </div>
+		    </div>
+			
+		    <div class="Actor-Momento  centrado4">
+	            <div class="container centrado4-titulo-controles">
+				 <h3>Actores del Momento</h3>
+				 <div class="indicadores"></div>
+		        </div>
+
+			    <div class="container centrado4-principal">
+				<button role="button" id="flecha-izquierdaa" class="flecha-izquierdaa"><i class="fas fa-angle-left"></i></button>
+
+				
+				 <div class="container centrado4-carousel">
+					   <div class="carousel">
+						   <div class="Actor">
+						     <a href="#"><img src="temp/A-3.jpg" alt=""></a>
+						   </div>
+						   <div class="Actor">
+						     <a href="#"><img src="temp/A-7.jpg" alt=""></a>
+						   </div>
+						   <div class="Actor">
+						     <a href="#"><img src="temp/A-4.jpg" alt=""></a>
+						   </div>
+						   <div class="Actor">
+						     <a href="#"><img src="temp/A-8.jpg" alt=""></a>
+						   </div>
+						   <div class="Actor">
+						     <a href="#"><img src="temp/A-5.jpg" alt=""></a>
+						   </div>
+						   <div class="Actor">
+						     <a href="#"><img src="temp/A-9.jpg" alt=""></a>
+						   </div>
+						   <div class="Actor">
+						     <a href="#"><img src="temp/A-6.jpg" alt=""></a>
+						   </div>
+						   <div class="Actor">
+						     <a href="#"><img src="temp/A-10.jpg" alt=""></a>
+						   </div>
+						   <div class="Actor">
+						     <a href="#"><img src="temp/A-11.jpg" alt=""></a>
+						   </div>
+						   <div class="Actor">
+						     <a href="#"><img src="temp/A-12.jpg" alt=""></a>
+						   </div>
+							
+					    </div>
+				
+                     <button role="button" id="flecha-derechaa" class="flecha-derechaa"><i class="fas fa-angle-right"></i></button>
+			        </div>
+		   </div>
+			
+		
+		</main>
+	 
+	
+		
+         <a class="botonD" href="actores.php">Mostrar Directores</a>
+		
+        <main>
+            <div class="Director-momento">
+			     <div class="container centrado5">
+				 <h5 class="titulo">Quentin Tarantino</h5>
+				 <p class="descripcion">
+				 Quentin Jerome Tarantino es un director, productor, guionista, escritor y editor estadounidense. Su carrera comenzó a finales de la década de 1980.
+				 </p>
+				 <button role="button" class="boton3"><i class="fas fa-info-circle"></i>Más Información</button>
+			    </div>
+		    </div>
+			
+		    <div class="Director-Momento  centrado5">
+	            <div class="container centrado4-titulo-controles">
+				 <h3>Director del Momento</h3>
+				 <div class="indicadores"></div>
+		        </div>
+
+			    <div class="container centrado5-principal">
+				<button role="button" id="flecha-izquierdaaa" class="flecha-izquierdaaa"><i class="fas fa-angle-left"></i></button>
+
+				
+				 <div class="container centrado5-carousel">
+					   <div class="carousel">
+						   <div class="Director">
+						     <a href="#"><img src="temp/D-1.jpg" alt=""></a>
+                            </div>
+						   <div class="Director">
+						     <a href="#"><img src="temp/D-3.jpg" alt=""></a>
+						   </div>
+						   <div class="Director">
+						     <a href="#"><img src="temp/D-4.jpg" alt=""></a>
+						   </div>
+						   <div class="Director">
+						     <a href="#"><img src="temp/D-5.jpg" alt=""></a>
+						   </div>
+						   <div class="Director">
+						     <a href="#"><img src="temp/D-6.jpg" alt=""></a>
+						   </div>
+						   <div class="Director">
+						     <a href="#"><img src="temp/D-7.jpg" alt=""></a>
+						   </div>
+						   <div class="Director">
+						     <a href="#"><img src="temp/D-8.jpg" alt=""></a>
+						   </div>
+						   <div class="Director">
+						     <a href="#"><img src="temp/D-9.jpg" alt=""></a>
+						   </div>
+					    </div>
+				
+                     <button role="button" id="flecha-derechaaa" class="flecha-derechaaa"><i class="fas fa-angle-right"></i></button>
+			        </div>
+		   </div>
+			
+		
+		</main>
+
+		
+		<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
+	     <script src="js/main.js"></script>
+
 </body>
 </html>
