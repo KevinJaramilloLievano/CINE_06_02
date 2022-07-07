d0d4873b7da2b4459183681ee1b337a35e9e8226
Angela Benitez <abh10@micorreo.upp.edu.mx>
2022-07-07 02:41:17 -0500

    Corrección en reporte de películas

--- reportePeliculas.php
+++ reportePeliculas.php
@@ -15,25 +15,17 @@
     //Generación de reporte
     $pdf = new FPDF();
 
-
 		//Ciclo para recorrer el objeto y sus datos
 		while($row = $resultado->fetch_assoc())
 		{
 		
 		    $pdf->AddPage();
-		    $pdf->SetFont('Arial','',20);
+		    $pdf->SetFont('Arial', 'BI', 20);
+			$pdf->Cell(190, 30, utf8_decode('CINE UPP "Películas"'), 0, 1, 'C');
 
-		    $pdf->Cell(190,30,'Peliculas CINE CUH',0,1,'C');
-
-		    $pdf->SetFont('Arial','',12);
-
-		    $pdf->SetXY(130,30);
-			$pdf->Cell(60,20,'Derechos reservados',0,1,'C');
-
-			$pdf->SetFont('Arial','',14);
-
-		    $pdf->SetXY(20,40);
-		    $pdf->Cell(170,20,'Nombre pelicula: ',0,1,'C');
+			$pdf->SetFont('Arial', '', 14);
+			$pdf->SetXY(20,40);
+		    $pdf->Cell(170,30, utf8_decode($row['nombrePelicula']).' - '.utf8_decode($row['nacionalidad']), 0, 1, 'C');
 
 		    $rutaImagen = 'C:/xampp/htdocs/CINE_06_02/temp/pelicula_'.$row['idPelicula'].'.png';
 		    $pdf->Image($rutaImagen,75,60,60,60);
@@ -42,10 +34,13 @@
 		    //Parámetros de la imagen:
 		    //url o localización del recurso
 		    //Posición X, Posición Y, Ancho y Alto
-		   // $pdf->Image('https://enriqueromerodominguezcine.com/wp-content/uploads/2020/01/Enrique-Romero-Cine-series-y-peliculas-de-netflix-logo.png',20,8,20,20);
+		    $pdf->Image('https://pbs.twimg.com/media/CUHMuS5UkAAjPpM.jpg',20,8,25,25);
+		    $pdf->Image('https://i.pinimg.com/originals/0c/32/97/0c3297f3516a415219c7e89e16a4a3d2.jpg',170,8,25,25);
 
-		    //$pdf->Image('https://static.vecteezy.com/system/resources/previews/002/412/416/non_2x/horror-films-studio-movie-cinema-film-production-logo-design-icon-illustration-vector.jpg',170,1,20,30);
+			$pdf->SetY(250);
+            $pdf->SetFont('Arial','I',8);
 
+            $pdf->Cell(0,10,utf8_decode('Página ').$pdf->PageNo().' - Derechos Reservados',0,0,'C');
 	    }
 
     $pdf->Output();
